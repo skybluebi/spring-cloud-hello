5bddfeea4b50f0a7115b3f7998c62efdbe197f0a
skybluebi <1337106710@qq.com>
2019-07-24 13:08:58 +0800

    fjajj

---
+++ Hello-spring-cloud/hello-spring-cloud-service-admin/src/main/java/com/funtl/hello/spring/cloud/service/admin/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.funtl.hello.spring.cloud.service.admin.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    @Value("${server.port}")
+    private String port;
+    @RequestMapping(value = "sayHi",method =RequestMethod.GET)
+    public String sayHi(String message){
+        return String.format("you message is %s,port is %s",message,port);
+    }
+}
---
+++ Hello-spring-cloud/hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/filter/loginFilter.java
@@ -0,0 +1,44 @@
+package com.funtl.hello.spring.cloud.zuul.filter;
+
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+@Component
+public class loginFilter extends com.netflix.zuul.ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        String token = request.getParameter("token");
+        if (token == null){
+            context.getResponse().setContentType("text/html;charset=utf-8");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                context.getResponse().getWriter().write("Token is empty");
+            } catch (IOException e) {
+            }
+
+        }
+        return null;
+    }
+}
